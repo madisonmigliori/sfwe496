72f1d12c63fec4a1f2e58e1352825b522cf1b9aa
madisonmigliori <madisonmigliori@arizona.edu>
2025-02-26 19:15:39 -0700

    Project Setup

---
+++ src/main/java/com/sfwe496/semesterproject/SemesterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sfwe496.semesterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemesterprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfwe496/semesterproject/SemesterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfwe496.semesterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

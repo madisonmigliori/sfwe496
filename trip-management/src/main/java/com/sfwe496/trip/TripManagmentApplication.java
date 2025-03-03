3cbffc3236bc7bea7f332a2ffd2b284e6c4b3ea9
madisonmigliori <madisonmigliori@arizona.edu>
2025-03-02 18:45:21 -0600

    Added trip-dev.properties

--- trip-management/src/main/java/com/sfwe496/trip/TripManagmentApplication.java
+++ trip-management/src/main/java/com/sfwe496/trip/TripManagmentApplication.java
@@ -1,4 +1,4 @@
-package com.sfwe496.semesterproject;
+package com.sfwe496.trip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

41abaac96bcff92a0782e31a7e48def2deaec9fe
madisonmigliori <madisonmigliori@arizona.edu>
2025-03-02 20:57:38 -0600

    updated applications

--- user/src/main/java/com/sfwe496/user/model/User.java
+++ user/src/main/java/com/sfwe496/user/model/User.java
@@ -1,9 +1,14 @@
 package com.sfwe496.user.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
+@Table(name = "users") // Avoids conflicts with SQL reserved keywords
 @Data  
 @NoArgsConstructor  
 @AllArgsConstructor 
@@ -13,8 +18,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String name;
-    private String email;
-    private String password;
-}
 
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+
+    public void setPassword(String password) {
+        this.password = new BCryptPasswordEncoder().encode(password);
+    }
+}

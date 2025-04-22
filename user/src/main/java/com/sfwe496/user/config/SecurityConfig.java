416a5b78b61b0adec38d8b3f7174e982631d4143
madisonmigliori <madisonmigliori@arizona.edu>
2025-04-21 21:20:56 -0700

    Update gateway

--- user/src/main/java/com/sfwe496/user/config/SecurityConfig.java
+++ user/src/main/java/com/sfwe496/user/config/SecurityConfig.java
@@ -1,26 +1,50 @@
-package com.sfwe496.user.config;
+package com.optimagrowth.user.config;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-
+    @Bean
+    public KeycloakAuthenticationProvider keycloakAuthenticationProvider() {
+        KeycloakAuthenticationProvider provider = new KeycloakAuthenticationProvider();
+        provider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        return provider;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/user/delete").hasRole("sie496-admin")
+                .requestMatchers("/user").hasRole("sie496-user")
                 .anyRequest().authenticated()
             )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt()
-            );
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+    @Bean
+    public NullAuthenticatedSessionStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
     }
 }
